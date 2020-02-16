3e3b5adaa135cf0ae901deb96a1f97f3ab5b7cff
meggyx2 <59489468+meggyx2@users.noreply.github.com>
2020-02-16 10:37:55 +0200

    Update botclean.js

--- commands/botclean.js
+++ commands/botclean.js
@@ -1,31 +1,11 @@
-import discord
-from discord.ext import commands
+module.exports.run = (bot, guild, message, args) => {
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return;
+    var total = 0;
+    var i = 0;
 
-import random
-import sys
-import traceback
-import asyncio
-import datetime
-import json
+    message.channel.fetchMessages().then(messages => {
+        total = messages.length;
 
-from datetime import datetime
-
-from cv import *
-
-@bot.command(aliases=["botclear", "botclean", "clean", "clear", "fuckoffbots"])
-@commands.has_permissions(manage_messages=True)
-async def bc(ctx):
-    def check(m):
-        return m.author.bot
-    await ctx.message.delete()
-    await ctx.message.channel.purge(limit=100, check=check)
-    await ctx.send("<a:speedclean:653273382812647425>")
-    
-@bc.error
-async def bc_error(error, ctx):
-    if isinstance(error, commands.CheckFailure):
-        await ctx.message.add_reaction("❌")
-        return
-    else:
-        print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
-        traceback.print_exception(type(error), error, None, file=sys.stderr)
+        messages.forEach(m => m.delete());
+    })
+}
